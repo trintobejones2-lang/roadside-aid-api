af33003ee82e58a4a9997b459b46fb668145e180
trintobe1 <trintobe.jones2@gmail.com>
2026-04-07 16:05:51 -0400

    update

--- src/volunteers/volunteers.module.ts
+++ src/volunteers/volunteers.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Volunteer } from './volunteer.entity';
 import { VolunteersController } from './volunteers.controller';
 import { VolunteersService } from './volunteers.service';
+import { PointsModule } from '../points/points.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Volunteer])],
+  imports: [TypeOrmModule.forFeature([Volunteer]), PointsModule],
   controllers: [VolunteersController],
   providers: [VolunteersService],
   exports: [VolunteersService],
